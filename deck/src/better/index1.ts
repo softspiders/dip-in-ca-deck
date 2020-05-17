38f143afab4f8137ff791c1f1f18a9514c643d8f
AlexanderLapygin <AlexanderLapygin@gmail.com>
2020-05-17 10:31:29 +0300

    docs: deck update

--- deck/src/better/index1.ts
+++ deck/src/better/index1.ts
@@ -1,7 +1,9 @@
 import { LoggerImpl } from "./LoggerImpl";
 import { CounterInteractor } from "./CounterInteractor";
+import { TimeLoggerImpl } from "./TimeLoggerImpl";
 
 const counter = new CounterInteractor(new LoggerImpl())
+//const counter = new CounterInteractor(new TimeLoggerImpl())
 
 counter.increment()
 counter.increment()
